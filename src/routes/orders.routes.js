6102fd417f694af37f33f2e950f724f014cfcb75
joao-sillva <joao.vgsillva@gmail.com>
2024-05-21 18:17:16 -0300

    list and edit order

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -7,10 +7,12 @@ const checkAdminPermission = require('../middlewares/checkAdminPermission')
 const ordersRoutes = Router()
 
 const ordersController = new OrdersController()
-
 ordersRoutes.use(ensureAuthenticated)
+
+ordersRoutes.get('/', ordersController.index);
 ordersRoutes.post('/', ordersController.create)
 ordersRoutes.get('/:id', ordersController.show)
 ordersRoutes.delete('/:id', checkAdminPermission, ordersController.delete)
+ordersRoutes.patch('/:id', checkAdminPermission, ordersController.update)
 
 module.exports = ordersRoutes
