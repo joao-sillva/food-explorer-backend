e73d9e1bbde0d3f230bec64a6e5d2d6dd1970c3d
joao-sillva <joao.vgsillva@gmail.com>
2024-05-21 01:38:30 -0300

    add database sqlite

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors')
+const database = require('./database/sqlite')
 const AppError = require("./utils/AppError")
 const express = require('express')
 const routes = require('./routes')
@@ -7,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+database()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
